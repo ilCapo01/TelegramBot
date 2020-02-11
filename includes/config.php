280e280cbafe921be6225071da83d8dacda99573
ilCapo01 <email@domain.com>
2020-02-11 18:26:38 +0200

    Added config.php

---
+++ includes/config.php
@@ -0,0 +1,11 @@
+<?php
+
+define('TIMEZONE', 'Asia/Jerusalem');
+define('AUTH_TOKEN', '865461164:AAG6F9tl0ewhfO41mmW0bP0pVPnyiV4r29g');
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'YOUR_DB_USERNAME');
+define('DB_PASS', 'YOUR_DB_PASSWORD');
+define('DB_NAME', 'YOUR_DB_NAME');
+
+?>
--- index.php
+++ index.php
@@ -1,60 +1,61 @@
 <?php
+ob_start();
 if ( !defined('ABSPATH'))
 	define('ABSPATH', dirname(__FILE__) . '/');
 
-define('TIMEZONE', 'Asia/Jerusalem');
+include(ABSPATH . 'includes/config.php');
+include(ABSPATH . 'includes/telegram.php');
+include(ABSPATH . 'includes/database.php');
+ob_end_flush();
 
-define('DB_HOST', 'localhost');
-define('DB_USER', 'YOUR_DB_USERNAME');
-define('DB_PASS', 'YOUR_DB_PASSWORD');
-define('DB_NAME', 'YOUR_DB_NAME');
+date_default_timezone_set(TIMEZONE);
 
 ini_set("log_errors", 1);
 ini_set("error_log", ABSPATH . 'php-error.log');
 
-date_default_timezone_set(TIMEZONE);
-
-include(ABSPATH . 'includes/telegram.php');
-include(ABSPATH . 'includes/database.php');
-
-$authToken = '865461164:AAG6F9tl0ewhfO41mmW0bP0pVPnyiV4r29g';
+$db = null;
 
 try {
 
 	$db = new Database(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-  $tg = new Telegram($authToken, $db);
+  $tg = new Telegram(AUTH_TOKEN, $db);
 
-	// Packet from telegram's user.
+	// Packet from Telegram user.
   $message = $tg->getMessage();
 
   $chat_id = $message->{'chat'}->{'id'};
   $text = $message->{'text'};
 
-  $userName = $message->{'from'}->{'first_name'} . ' ' . $message->{'from'}->{'last_name'};
-
 	// Start command.
   if ($tg->setCommand('/start'))
   {
+
 		// TODO: Bug; the user doesnt receive the image.
 		$tg->sendPhoto($chat_id, 'D4jmpIlU0AAhMY9.png');
 
 		// Send the keyboard.
-	  $tg->sendKeyboardMarkup($chat_id, 'Hello, ' . $userName . '.', array(
+	  $tg->sendKeyboardMarkup($chat_id, 'Hello, ' .
+		$message->{'from'}->{'first_name'} . ' ' .
+		$message->{'from'}->{'last_name'} . '.', array(
 			'Option #1', 'Option #2', 'Option #3'
 	  ));
   }
-	else if ($tg->setCommand('Option #1')) {
+	else if ($tg->setCommand('Option #1'))
+	{
 		$tg->sendMessage($chat_id, 'You picked Option #1');
 	}
-	else if ($tg->setCommand('Option #2')) {
+	else if ($tg->setCommand('Option #2'))
+	{
 		$tg->sendMessage($chat_id, 'You picked Option #2');
 	}
-	else if ($tg->setCommand('Option #3')) {
+	else if ($tg->setCommand('Option #3'))
+	{
 		$tg->sendMessage($chat_id, 'You picked Option #3');
 	}
 
 } catch (Exception $e) {
   error_log($e->getMessage());
 }
+$db->close();
 
 ?>
