d73861fb141f6201e163137eff4539b094127fea
ilCapo01 <2365754+ilCapo01@users.noreply.github.com>
2020-02-23 03:33:41 +0200

    Update mybot.php

--- includes/mybot.php
+++ includes/mybot.php
@@ -21,25 +21,25 @@ class MyBot extends Bot {
 	  {
 			// Send the keyboard.
 		  $tg->sendKeyboardMarkup($chat_id, 'Hello, ' .
-			$message->{'from'}->{'first_name'} . ' ' .
-			$message->{'from'}->{'last_name'} . '.', array(
+			$this->msg->{'from'}->{'first_name'} . ' ' .
+			$this->msg->{'from'}->{'last_name'} . '.', array(
 				'Option #1', 'Option #2', 'Option #3'
 		  ));
       return true;
 	  }
 		else if (parent::onCommand('Option #1'))
 		{
-			$tg->sendMessage($chat_id, 'You picked Option #1');
+			$this->tg->sendMessage($chat_id, 'You picked Option #1');
       return true;
 		}
 		else if (parent::onCommand('Option #2'))
 		{
-			$tg->sendMessage($chat_id, 'You picked Option #2');
+			$this->tg->sendMessage($chat_id, 'You picked Option #2');
       return true;
 		}
 		else if (parent::onCommand('Option #3'))
 		{
-			$tg->sendMessage($chat_id, 'You picked Option #3');
+			$this->tg->sendMessage($chat_id, 'You picked Option #3');
       return true;
 		}
     return false;
